012c2786230d8680987e4f4d177f52e74481263d
pc-jponsekar <jebastian.ponsekar@personalcapital.com>
2014-11-01 16:35:07 -0700

    Added login controller

---
+++ src/main/java/com/proximityperks/model/ProxmityPerksJsonModel.java
@@ -0,0 +1,29 @@
+package com.proximityperks.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProxmityPerksJsonModel {
+	private Object spHeader = new Object();
+	private Object spData = new Object();
+
+
+
+	public Object getSpHeader() {
+		return spHeader;
+	}
+
+	public void setSpHeader(Object spHeader) {
+		this.spHeader = spHeader;
+	}
+
+	public Object getSpData() {
+		return spData;
+	}
+
+	public void setSpData(Object spData) {
+		this.spData = spData;
+	}
+}
