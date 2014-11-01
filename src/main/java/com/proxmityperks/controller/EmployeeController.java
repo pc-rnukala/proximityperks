72eec073a155c6f6201ff2f30d4383d70c4c265f
pc-jponsekar <jebastian.ponsekar@personalcapital.com>
2014-11-01 13:56:56 -0700

    db manager connection success from AWS

--- src/main/java/com/proxmityperks/controller/EmployeeController.java
+++ src/main/java/com/proxmityperks/controller/EmployeeController.java
@@ -1,16 +1,19 @@
 package com.proxmityperks.controller;
 
+import java.sql.Connection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.proximityperks.manager.ProximityPerkDbManager;
 import com.proximityperks.model.Employee;
 
 @Controller
@@ -18,6 +21,9 @@ public class EmployeeController {
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(EmployeeController.class);
+
+	@Autowired
+	private ProximityPerkDbManager proximityPerkDbManager;
 
 	// Map to store employees, ideally we should use database
 	Map<Integer, Employee> empData = new HashMap<Integer, Employee>();
@@ -28,10 +34,20 @@ public class EmployeeController {
 		logger.info("Start getDummyEmployee");
 		Employee emp = new Employee();
 		emp.setId(9999L);
-		emp.setName("Dummy");
+		Connection connection;
+		boolean status=false;
+		try {
+			connection = proximityPerkDbManager.getConnection();
+			status = connection != null ? true : false;
+			connection.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		emp.setName("Dummy"+"connection status"+status);
 		emp.setCreatedDate(new Date());
 		empData.put(9999, emp);
 		return emp;
 	}
-
 }
