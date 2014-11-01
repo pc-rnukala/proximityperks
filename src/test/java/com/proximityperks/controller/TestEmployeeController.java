49e89e3ba4e36adb8ede75e85ca7e7835db693f2
pc-rnukala <ravi.nukala@personalcapital.com>
2014-11-01 12:20:23 -0700

    All project settings

---
+++ src/test/java/com/proximityperks/controller/TestEmployeeController.java
@@ -0,0 +1,14 @@
+package com.proximityperks.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestEmployeeController {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
